b953968e6f4912e8e583f1b79cdf0d95aae70c9b
PhucEGN <lehoangphucto@gmail.com>
2024-07-28 11:15:40 +0700

    update 7 and 8

---
+++ 8.String.py
@@ -0,0 +1,13 @@
+Str = "This is a String"
+
+print("Example 1:")
+# lấy ra 1 kí tự trong chuỗi // get character at position 6
+print(Str[6])
+
+print("Example 2:")
+# lấy ra các kí tự trong Str
+for x in Str:
+    print(x)
+
+print("Example 3:")
+print(len(Str))
